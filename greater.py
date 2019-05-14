a1569f710a92514ca6ed4110fbcc2c7b657727b0
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-05-14 16:31:11 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,10 +1,12 @@
-a=int(input())
-b=int(input())
-c=int(input())
+a=input()
+b=input()
+c=input()
 if a > b and a > c:
     print(a)
 elif b > a and b > c:
     print(b)
-else:
+elif c > a and c > b:
     print(c)
+else:
+    print("Error")
     
