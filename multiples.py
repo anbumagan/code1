3c1d43b607385914ecfffbcbe9837992d3faa4f3
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-07 00:52:20 -0700

    Update multiples.py

--- multiples.py
+++ multiples.py
@@ -1,9 +1,5 @@
 a=int(input())
-if(a==0):
-    print("0 0 0 0 0") 
-    break
-sum=0
-for i in range(1,a+1):
-    sum=a*i
-    print(sum,"",end="")    
+for i in range(1,6):
+    n=a*i
+    print(n,"",end="")    
    
